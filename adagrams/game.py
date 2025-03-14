b76c77518b12483ae4b6acd1c26d328d3af31a08
KateM <ekaterinamarantidi@gmail.com>
2025-03-13 21:37:41 -0700

    wave 1 passes

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,49 @@
 from random import randint
+import random
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+LETTERS_LIST = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
+
 
 def draw_letters():
-    pass
+    
+    tile = []
+    for x in range(10):
+        random_integer = random.randint(0, len(LETTERS_LIST)-1)
+        letter = LETTERS_LIST[random_integer]
+        if tile.count(letter) == LETTER_POOL[letter]:
+            break
+        tile.append(LETTERS_LIST[random_integer])
+    return tile
+
+    
 
 def uses_available_letters(word, letter_bank):
     pass
